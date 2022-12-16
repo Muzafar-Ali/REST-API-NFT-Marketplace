d2c0f23da4f2d3430ff0f7b46fb69ce08ebf650d
Muzafar-Ali <muzafar.ali.chandio@gmail.com>
2022-12-17 02:35:31 +0500

    added middleware for NFT id check function for createNft
    1. created separate middleware for NFT id request
    2. created check function for createNft as users should provide correct and required data, made name and price mandatory

--- controllers/nftControllers.js
+++ controllers/nftControllers.js
@@ -3,6 +3,37 @@ const fs = require('fs')
 const nfts = JSON.parse(fs.readFileSync(`${__dirname}/../nft-data/data/nft-simple.json`)) //coverted to JSON object
 const addNfts = JSON.parse(fs.readFileSync(`${__dirname}/../nft-data/data/addData.json`)) //coverted to JSON object
 
+//middleware for NFT id request
+exports.checkId = ((req, res, next, value)=>{
+    console.log(`ID: ${value}`)
+    if(req.params.id * 1 > addNfts.length){
+        return res.status(404).json({
+            status:"fail",
+            message:"No NFT with such id"
+        })
+    }
+    // //OR we can write logic as follow 
+    // const receivedId = Number(req.params.id)
+    // const nft = addNfts.find((x)=> x.id === receivedId);
+    // if(!nft){
+    //   return res.status(404).json({
+    //     status:"Failed",
+    //     message:"No NFT with such id"
+    //   })
+    // }   
+    next();
+})
+
+exports.checkBody = ((req, res, next)=>{
+    if(!req.body.name || !req.body.price){
+        return res.status(400).json({
+            status:'Fail',
+            message:'Missing nams and price'
+        })
+    }
+    next();
+})
+
 //create get Request All NFTS
 exports.getAllNfts = ((req,res)=>{
   res.status(200).json({
@@ -70,15 +101,7 @@ exports.createNft = ((req,res)=>{
   
   // cretae DELETE Method
   exports.deteleNfts = ((req,res)=>{
-    // just testing will add logic later
-    const receivedId = Number(req.params.id)
-    const nft = addNfts.find((x)=> x.id === receivedId);
-    if(!nft){
-      return res.status(404).json({
-        status:"Failed",
-        message:"No NFT with such id"
-      })
-    }
+
     res.status(200).json({
       status:'success',
       message:'DELETE NFT Price'
--- routes/nftsRoute.js
+++ routes/nftsRoute.js
@@ -3,7 +3,13 @@ const nftConrtollers = require('../controllers/nftControllers')
 
 const router = express.Router();
 
-router.route('/').get(nftConrtollers.getAllNfts).post(nftConrtollers.createNft)
+//params middleware for NFT id call
+router.param("id",nftConrtollers.checkId)
+
+router.route('/')
+.get(nftConrtollers.getAllNfts)
+.post(nftConrtollers.checkBody, nftConrtollers.createNft)
+  
 router.route('/:id').get(nftConrtollers.getSingleNft).patch(nftConrtollers.updateNfts).delete(nftConrtollers.deteleNfts)
 
 module.exports = router 
