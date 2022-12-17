564793fbeac30b567d29a143da2bfa153bb5e22a
Muzafar-Ali <muzafar.ali.chandio@gmail.com>
2022-12-18 00:02:12 +0500

    create database mongoDB
    create database mongoDB, connect datatbase with application, installed mongoose

--- server.js
+++ server.js
@@ -1,7 +1,23 @@
-const dotenv = require('dotenv');
 const app = require('./app');
+const dotenv = require('dotenv');
+const mongoose = require('mongoose')
 
 dotenv.config({path:"./config.env"})
+
+const DB = process.env.DATABASE.replace("<PASSWORD>",process.env.DATABASE_PASSWORD)
+//console.log(app.get("env"));
+
+mongoose.connect(DB,{
+    useCreateIndex:true,
+    useFindAndModify:false,
+    useNewUrlParser:true
+})
+.then((con)=>{
+    //console.log(con.connection)
+    console.log("DB Connected Succesfully")
+
+})
+
 
 //create port we can add any number 5000/3000
 const port = process.env.PORT || 3000;
